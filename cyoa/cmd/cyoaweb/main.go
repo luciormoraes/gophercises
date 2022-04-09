5713a8225b7461dcf68476f7af292acdcac54d38
Lucio Moraes <luciomoraes@gmail.com>
2022-04-09 16:26:34 +1200

    Builiding http.Handler

--- cyoa/cmd/cyoaweb/main.go
+++ cyoa/cmd/cyoaweb/main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 
 	"github.com/luciormoraes/gophercises/cyoa"
 )
 
 func main() {
+	port := flag.Int("port", 3001, "the port to start the CYOA web application on")
 	filename := flag.String("filename", "gopher.json", "the JSON filename with the CYOA story")
 	flag.Parse()
 	fmt.Printf("Using the story in %s.\n", *filename)
@@ -22,5 +25,8 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("%+v\n", story)
+	// fmt.Printf("%+v\n", story)
+	h := cyoa.NewHandler(story)
+	fmt.Printf("Starting the server at: %d\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf("%d", *port), h))
 }
--- cyoa/story.go
+++ cyoa/story.go
@@ -2,8 +2,52 @@ package cyoa
 
 import (
 	"encoding/json"
+	"html/template"
 	"io"
+	"net/http"
 )
+
+// func init() {
+// 	tpl := template.Must(template.New("").Parse(defaultHandlerTmpl))
+// }
+
+var tpl *template.Template
+
+var defaultHandlerTmpl = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Choose Your Own Adventure</title>
+</head>
+<body>
+    <h1>{{.Title}}</h1>
+    {{range .Paragraphs}}
+    <p>{{.}}</p>
+    {{end}}
+    {{range .Options}}
+    <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+    {{end}}
+</body>
+</html>`
+
+func NewHandler(s Story) http.Handler {
+	return handler{s}
+}
+
+type handler struct {
+	s Story
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// tpl := template.Must(template.New("").Parse(defaultHandlerTmpl))
+	err := tpl.Execute(w, h.s["intro"])
+	if err != nil {
+		panic(err)
+	}
+}
 
 func JsonStory(r io.Reader) (Story, error) {
 	d := json.NewDecoder(r)
