95ad8846cba42f489de2bf010f210ca7a15381e9
Lucio Moraes <luciomoraes@gmail.com>
2022-04-09 15:57:42 +1200

    Refactoring: creating function to parse json

--- cyoa/story.go
+++ cyoa/story.go
@@ -1,5 +1,19 @@
 package cyoa
 
+import (
+	"encoding/json"
+	"io"
+)
+
+func JsonStory(r io.Reader) (Story, error) {
+	d := json.NewDecoder(r)
+	var story Story
+	if err := d.Decode(&story); err != nil {
+		panic(err)
+	}
+	return story, nil
+}
+
 type Story map[string]Chapter
 
 type Chapter struct {
