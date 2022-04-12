b3974a6c4917fdcea0f8c697edb8c862b20cbfd2
Lucio Moraes <luciomoraes@gmail.com>
2022-04-12 22:24:21 +1200

    Gophercises: Choose your own adventure * Custom templates * Functional Options - https://dave.cheney.net/2014/10/17/functional-options-for-friendly-apis * Custom Paths

--- cyoa/cmd/cyoaweb/main.go
+++ cyoa/cmd/cyoaweb/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/luciormoraes/gophercises/cyoa"
 )
@@ -25,10 +27,94 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
 	// fmt.Printf("%+v\n", story)
-	// tpl := template.Must(template.New("").Parse(storyTmpl))
-	h := cyoa.NewHandler(story, nil)
+	// tpl := template.Must(template.New("").Parse("Hello!"))
+
+	// h := cyoa.NewHandler(story, cyoa.WithTemplate(tpl))
+	tpl := template.Must(template.New("").Parse(storyTmpl))
+	h := cyoa.NewHandler(story,
+		cyoa.WithTemplate(tpl),
+		cyoa.WithPathFunc(pathFn),
+	)
+
+	mux := http.NewServeMux()
+	mux.Handle("/story/", h)
+
 	fmt.Printf("Starting the server at: %d\n", *port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), mux))
 	// http.ListenAndServe(fmt.Sprintf("%d", *port), h)
 }
+
+func pathFn(r *http.Request) string {
+	path := strings.TrimSpace(r.URL.Path)
+	if path == "/story" || path == "/story/" {
+		path = "/story/intro"
+	}
+
+	return path[len("/story/"):]
+}
+
+var storyTmpl = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Choose Your Own Adventure</title>
+</head>
+<body>
+    <section class="page">
+		<h1>{{.Title}}</h1>
+		{{range .Paragraphs}}
+		<p>{{.}}</p>
+		{{end}}
+		<ul>
+		{{range .Options}}
+		<li><a href="/story/{{.Chapter}}">{{.Text}}</a></li>
+		{{end}}
+		<ul>
+	</section>
+	<style>
+      body {
+        font-family: helvetica, arial;
+      }
+      h1 {
+        text-align:center;
+        position:relative;
+      }
+      .page {
+        width: 80%;
+        max-width: 500px;
+        margin: auto;
+        margin-top: 40px;
+        margin-bottom: 40px;
+        padding: 80px;
+        background: #FCF6FC;
+        border: 1px solid #eee;
+        box-shadow: 0 10px 6px -6px #797;
+      }
+      ul {
+        border-top: 1px dotted #ccc;
+        padding: 10px 0 0 0;
+        -webkit-padding-start: 0;
+      }
+      li {
+        padding-top: 10px;
+      }
+      a,
+      a:visited {
+        text-decoration: underline;
+        color: #555;
+      }
+      a:active,
+      a:hover {
+        color: #222;
+      }
+      p {
+        text-indent: 1em;
+      }
+    </style>
+</body>
+</html>`
